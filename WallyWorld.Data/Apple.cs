de535bdf9af0b19085f2aded472bfa723f91c811
Terry Brown <efa.terrybrown@gmail.com>
2022-04-16 13:35:29 -0400

    FIN01

--- WallyWorld.Data/Apple.cs
+++ WallyWorld.Data/Apple.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
+//? 1. Data layer
+//? 2. Repository Layer
+//? 3. UI layer
+
 public class Apple : Produce
 {
     public Apple(DateTime dateOfArrival)
